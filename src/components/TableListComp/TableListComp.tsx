8bccf057636afaf1833e6e7e0c2f0638a9df305d
Loki99911 <nikita_kresik@i.ua>
2023-09-03 16:52:04 +0300

    Update TableListComp.tsx

--- src/components/TableListComp/TableListComp.tsx
+++ src/components/TableListComp/TableListComp.tsx
@@ -55,24 +55,6 @@ function createData(
   };
 }
 
-
-
-// const rows = [
-//   createData("Cupcake", 305, 3.7, 67, 4.3),
-//   createData("Donut", 452, 25.0, 51, 4.9),
-//   createData("Eclair", 262, 16.0, 24, 6.0),
-//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-//   createData("Gingerbread", 356, 16.0, 49, 3.9),
-//   createData("Honeycomb", 408, 3.2, 87, 6.5),
-//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-//   createData("Jelly Bean", 375, 0.0, 94, 0.0),
-//   createData("KitKat", 518, 26.0, 65, 7.0),
-//   createData("Lollipop", 392, 0.2, 98, 0.0),
-//   createData("Marshmallow", 318, 0, 81, 2.0),
-//   createData("Nougat", 360, 19.0, 9, 37.0),
-//   createData("Oreo", 437, 18.0, 63, 4.0),
-// ];
-
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
     return -1;
@@ -120,8 +102,6 @@ interface HeadCell {
 }
 
 const headCells: readonly HeadCell[] = [
-  // ID, назва, опис, ціна, фото, рейтинг, сток, категорія
-  // id,title,description,price,images,rating,stock,category
   {
     id: "id",
     numeric: false,
@@ -281,9 +261,13 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
 }
 
 export default function TableListComp() {
+  const [order, setOrder] = React.useState<Order>("asc");
+  const [orderBy, setOrderBy] = React.useState<keyof Data>("id");
+  const [selected, setSelected] = React.useState<readonly string[]>([]);
+  const [page, setPage] = React.useState(0);
+  // const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
   const rowsReq = useAppSelector((state) => state.products.products);
-  console.log(rowsReq);
-  
   const rows = rowsReq.map(
     ({ id, title, description, price, images, rating, stock, category }) => {
       return createData(
@@ -298,13 +282,6 @@ export default function TableListComp() {
       );
     }
   );
-  
-  const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof Data>("id");
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
-  const [page, setPage] = React.useState(0);
-  // const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   const handleRequestSort = (
     event: React.MouseEvent<unknown>,
@@ -429,7 +406,9 @@ export default function TableListComp() {
                     <TableCell align="left">{row.title}</TableCell>
                     <TableCell align="left">{row.description}</TableCell>
                     <TableCell align="left">{row.price}</TableCell>
-                    <TableCell align="left">{row.images}</TableCell>
+                    <TableCell align="left">
+                      <img src={row.images[0]} alt={row.title} style={{width:100,height:50}}/>
+                    </TableCell>
                     <TableCell align="left">{row.rating}</TableCell>
                     <TableCell align="left">{row.stock}</TableCell>
                     <TableCell align="left">{row.category}</TableCell>
